9b38d8a24e986c135e211ce0e4459bc1eec4769e
Datta_Varsha <dattv186@hsd.k12.or.us>
2017-05-09 08:55:37 -0700

    updating files

--- ConeVolumeCalculator.py
+++ ConeVolumeCalculator.py
@@ -4,11 +4,26 @@
 # factor based on the Volume Calculator
 # Calculator.net (http://www.calculator.net/volume-calculator.html)
 
+
 # Define Function below
 # be sure to return an integer
+
+import math
+
+def calculateConeVolume(baseRadius, height):
+    calculateConeVolume = (1/3) * math.pi * baseRadius**2 * height
+    calculateConeVolume = round(float(calculateConeVolume), 2)
+    return(calculateConeVolume)
+    
+    
+
 
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
-    pass # remove or comment out this line if you wish to test the function
+    baseRadius = float(input('Enter the raduis of the cone:    '))
+    height =  float(input('Enter the height of the cone:    '))
+    volume = calculateConeVolume(baseRadius, height)
+    print(volume)
+    
